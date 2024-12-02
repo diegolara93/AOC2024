d2e6894ae91fee40daeed98392bad2fd44b74457
diego <diegolara93345@gmail.com>
2024-12-02 00:00:08 -0600

    day one finished

--- Day1/main/src/main.rs
+++ Day1/main/src/main.rs
@@ -3,15 +3,30 @@ use std::io::prelude::*;
 use std::vec;
 
 fn main() -> std::io::Result<()> {
+    let mut ans: i32 = 0;
     let mut file = File::open("./input.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     let new_text = contents.split_whitespace();
     let mut nums: Vec<i32> = vec![];
+    let mut nums1: Vec<i32> = vec![];
+    let mut nums2: Vec<i32> = vec![];
     for words in new_text {
-        println!("{}", words);
         nums.push(words.parse::<i32>().unwrap());
     }
-    print!("{}", nums.len());
+    for i in 0..nums.len() {
+        if i % 2 == 0 {
+            nums1.push(nums[i]);
+        }
+        else {
+            nums2.push(nums[i]);
+        }
+    }
+    nums1.sort();
+    nums2.sort();
+    for i in 0..nums1.len() {
+        ans += (nums1[i] - nums2[i]).abs();
+    }
+    println!("The answer for day one is: {}", ans);
     Ok(())
 }
